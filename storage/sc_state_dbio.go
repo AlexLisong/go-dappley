b624ee5e851b75b57ee920469119ba7ce2997cfe
chaoswlz <wulize1994@gmail.com>
2019-09-27 11:07:44 -0700

    add changelog get, delete and save function

--- storage/sc_state_dbio.go
+++ storage/sc_state_dbio.go
@@ -1,6 +1,8 @@
 package storage
 
 import (
+	"github.com/dappley/go-dappley/common/hash"
+
 	"github.com/dappley/go-dappley/core/scState"
 )
 
@@ -31,4 +33,27 @@ func (dbio *SCStateDBIO) LoadScStateFromDatabase() *scState.ScState {
 //SaveToDatabase saves states to database
 func (dbio *SCStateDBIO) SaveToDatabase(ss *scState.ScState) error {
 	return dbio.db.Put([]byte(scStateMapKey), ss.Serialize())
+}
+
+func (dbio *SCStateDBIO) GetChangeLog(prevHash hash.Hash) map[string]map[string]string {
+	change := make(map[string]map[string]string)
+
+	rawBytes, err := dbio.db.Get([]byte(scStateLogKey + prevHash.String()))
+
+	if err != nil && err.Error() == ErrKeyInvalid.Error() || len(rawBytes) == 0 {
+		return change
+	}
+	change = scState.DeserializeChangeLog(rawBytes).Log
+
+	return change
+}
+
+func (dbio *SCStateDBIO) DeleteChangeLog(prevHash hash.Hash) error {
+	err := dbio.db.Del([]byte(scStateLogKey + prevHash.String()))
+	return err
+}
+
+func (dbio *SCStateDBIO) SaveChangeLog(change *scState.ChangeLog, blkHash hash.Hash) error {
+	err := dbio.db.Put([]byte(scStateLogKey+blkHash.String()), change.SerializeChangeLog())
+	return err
 }
