f8e255c9d99bf5e13216f6357d59999596b017bd
chaoswlz <wulize1994@gmail.com>
2019-09-30 15:46:52 -0700

    add db function to tx journal dbio

--- storage/tx_journal_dbio.go
+++ storage/tx_journal_dbio.go
@@ -1,9 +1,24 @@
 package storage
 
+import (
+	"github.com/dappley/go-dappley/core/transaction"
+)
+
 type TXJournalDBIO struct {
 	db Storage
 }
 
 func NewTXJournalDBIO(db Storage) *TXJournalDBIO {
 	return &TXJournalDBIO{db: db}
+}
+
+func (dbio *TXJournalDBIO) PutTxJournal(tx transaction.Transaction) error {
+	txJournal := transaction.NewTxJournal(tx.ID, tx.Vout)
+
+	bytes, err := txJournal.SerializeJournal()
+	if err != nil {
+		return err
+	}
+	err = dbio.db.Put(GetStorageKey(txJournal.Txid), bytes)
+	return err
 }
