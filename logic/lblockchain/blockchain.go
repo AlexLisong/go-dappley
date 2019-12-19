bcdf3401a1a144ba9f1b621af3c4f72f08336011
fly <fly1992@163.com>
2019-12-19 15:22:39 +0800

    modified check repeat block

--- logic/lblockchain/blockchain.go
+++ logic/lblockchain/blockchain.go
@@ -541,6 +541,14 @@ func (bc *Blockchain) checkRepeatingProducer(blk *block.Block) bool {
 			return false
 		}
 
+		logger.WithFields(logger.Fields{
+			"currBlkHeight": currBlk.GetHeight(),
+			"producer":      currBlk.GetProducer(),
+			"hash":          currBlk.GetHash(),
+			"libHash":       lib.String(),
+			"index":			i,
+		}).Info("Blockchain: Info")
+
 		if _, ok := existProducers[currBlk.GetProducer()]; ok {
 			logger.WithFields(logger.Fields{
 				"currBlkHeight": currBlk.GetHeight(),
